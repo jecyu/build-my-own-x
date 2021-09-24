e70629dfabb82645338f7985a82c05e6da7e0513
Jecyu <jecyu.lin@gmail.com>
2021-09-24 18:31:08 +0800

    feat(emitter): off 支持移除多个 listener、添加 once、listeners、hasListeners

--- emitter/index.js
+++ emitter/index.js
@@ -20,6 +20,50 @@
     }
   };
 
+  Emitter.prototype.off = function (event, fn) {
+    if (arguments.length === 0) {
+      // 移除所有 listener
+      this.eventHandlerMap.clear();
+    } else if (arguments.length === 1) {
+      // 移除 event 下所有的 listener
+      const fns = this.eventHandlerMap.get(event);
+      fns.length = 0;
+    } else if (arguments.length === 2) {
+      const fns = this.eventHandlerMap.get(event);
+      if (fns && fns.length) {
+        const index = fns.findIndex(
+          (listener) => listener === fn || listener.fn === fn
+        );
+        index > -1 && fns.splice(index, 1);
+      }
+    }
+  };
+
+  // 注册一个单次事件处理程序 fn，在它第一次被调用后立即删除
+  Emitter.prototype.once = function (event, fn) {
+    const callback = (...args) => {
+      typeof fn === 'function' && fn(...args);
+      this.off(event, callback);
+    };
+    callback.fn = fn;
+    this.on(event, callback);
+  };
+
+  // 获取 event 下所有的 listener
+  Emitter.prototype.listeners = function (event) {
+    const fns = this.eventHandlerMap.get(event);
+    if (fns && fns.length) {
+      return fns;
+    } else {
+      return [];
+    }
+  };
+
+  Emitter.prototype.hasListeners = function (event) {
+    const fns = this.eventHandlerMap.get(event);
+    return !!(fns && fns.length);
+  };
+
   //AMD.
   if (typeof define === 'function' && define.amd) {
     define(function () {
--- emitter/test/emitter.test.js
+++ emitter/test/emitter.test.js
@@ -1,8 +1,8 @@
 const Emitter = require('..');
 
-describe('Emitter', function () {
-  describe('.on(event, fn)', function () {
-    it('should add listeners', function () {
+describe('Emitter', () => {
+  describe('.on(event, fn)', () => {
+    it('should add listeners', () => {
       const emitter = new Emitter();
       const calls = [];
 
@@ -18,8 +18,148 @@ describe('Emitter', function () {
       emitter.emit('bar', 1);
       emitter.emit('foo', 2);
 
-      console.log('calls ->', calls)
       expect(calls).toEqual(['one', 1, 'two', 1, 'one', 2, 'two', 2]);
+    });
+  });
+
+  describe('.off(event, fn)', () => {
+    it('should remove a listener', () => {
+      const emitter = new Emitter();
+      const calls = [];
+
+      function one() {
+        calls.push('one');
+      }
+      function two() {
+        calls.push('two');
+      }
+
+      emitter.on('foo', one);
+      emitter.on('foo', two);
+      emitter.off('foo', two);
+
+      emitter.emit('foo');
+
+      expect(calls).toEqual(['one']);
+    });
+
+    it('should work with .once()', () => {
+      const emitter = new Emitter();
+      const calls = [];
+
+      function one() {
+        calls.push('one');
+      }
+
+      emitter.once('foo', one);
+      emitter.once('fee', one);
+      emitter.off('foo', one);
+
+      emitter.emit('foo');
+
+      expect(calls).toEqual([]);
+    });
+  });
+
+  describe('.off(event)', () => {
+    it('should remove all listeners for an event', () => {
+      const emitter = new Emitter();
+      const calls = [];
+      function one() {
+        calls.push('one');
+      }
+      function two() {
+        calls.push('two');
+      }
+
+      emitter.on('foo', one);
+      emitter.on('foo', two);
+      emitter.off('foo');
+
+      emitter.emit('foo');
+      emitter.emit('foo');
+      expect(calls).toEqual([]);
+    });
+  });
+
+  describe('.off()', () => {
+    it('should remove all listeners', () => {
+      const emitter = new Emitter();
+      const calls = [];
+
+      function one() {
+        calls.push('one');
+      }
+
+      function two() {
+        calls.push('two');
+      }
+
+      emitter.on('foo', one);
+      emitter.on('bar', two);
+
+      emitter.emit('foo');
+      emitter.emit('bar');
+
+      emitter.off();
+
+      emitter.emit('foo');
+      emitter.emit('bar');
+
+      expect(calls).toEqual(['one', 'two']);
+    });
+  });
+
+  describe('.once(event, fn)', () => {
+    it('should add a single-shot listener', () => {
+      const emitter = new Emitter();
+      const calls = [];
+
+      emitter.once('foo', function (val) {
+        calls.push('one', val);
+      });
+
+      emitter.emit('foo', 1);
+      emitter.emit('foo', 2);
+      emitter.emit('foo', 3);
+      emitter.emit('bar', 1);
+
+      expect(calls).toEqual(['one', 1]);
+    });
+  });
+
+  describe('.listeners(event)', () => {
+    describe('when handlers are present', () => {
+      it('should return an array of callbacks', () => {
+        const emitter = new Emitter();
+        function foo() {}
+        emitter.on('foo', foo);
+        expect(emitter.listeners('foo')).toEqual([foo]);
+      });
+    });
+
+    describe('when no handlers are present', () => {
+      it('should return an empty array', () => {
+        const emitter = new Emitter();
+        expect(emitter.listeners('foo')).toEqual([]);
+      });
+    });
+  });
+
+  describe('.hasListeners(event)', () => {
+    describe('when handlers are present', () => {
+      it('should return true', () => {
+        const emitter = new Emitter();
+        emitter.on('foo', () => {});
+        expect(emitter.hasListeners('foo')).toBe(true);
+      });
+    });
+
+    describe('when no handlers are present', () => {
+      it('should return false', () => {
+        const emitter = new Emitter();
+        expect(emitter.hasListeners('foo')).toBe(false);
+      });
     });
   });
 });
