7576482db1f1a263d489a985a90867e8288c2f45
Jecyu <jecyu.lin@gmail.com>
2021-12-13 22:25:18 +0800

    feat(emitter): 优化 emitter 初始化和 off 逻辑

--- emitter/index.js
+++ emitter/index.js
@@ -1,18 +1,19 @@
 ;(function (GLOBAL) {
   // 注册事件处理器
   function mixin(obj) {
-    for (let key in Emitter.prototype) {
+    for (const key in Emitter.prototype) {
       obj[key] = Emitter.prototype[key]
+      obj.eventHandlerMap = new Map()
     }
     return obj
   }
   function Emitter(obj) {
+    this.eventHandlerMap = new Map()
     if (obj) return mixin(obj)
   }
 
   // 注册事件处理器
   Emitter.prototype.on = function (event, fn) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     if (this.eventHandlerMap.has(event)) {
       typeof fn === 'function' && this.eventHandlerMap.get(event).push(fn)
     } else {
@@ -22,15 +23,13 @@
 
   // 发射一个带有可变选项 args 的事件
   Emitter.prototype.emit = function (event, ...args) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     const fns = this.eventHandlerMap.get(event)
     if (fns && fns.length) {
-      fns.forEach((fn) => fn(...args))
+      fns.forEach((fn) => fn && fn(...args))
     }
   }
 
   Emitter.prototype.off = function (event, fn) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     if (arguments.length === 0) {
       // 移除所有 listener
       this.eventHandlerMap.clear()
@@ -44,14 +43,19 @@
         const index = fns.findIndex(
           (listener) => listener === fn || listener.fn === fn
         )
-        index > -1 && fns.splice(index, 1)
+        if (index > -1) {
+          fns[index] = null // 避免改变 fns 长度，导致 emit 遍历错误
+          // 延时删除
+          setTimeout(() => {
+            fns.splice(index, 1)
+          }, 0)
+        }
       }
     }
   }
 
   // 注册一个单次事件处理程序 fn，在它第一次被调用后立即删除
   Emitter.prototype.once = function (event, fn) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     const callback = (...args) => {
       typeof fn === 'function' && fn(...args)
       this.off(event, callback)
@@ -62,7 +66,6 @@
 
   // 获取 event 下所有的 listener
   Emitter.prototype.listeners = function (event) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     const fns = this.eventHandlerMap.get(event)
     if (fns && fns.length) {
       return fns
@@ -72,7 +75,6 @@
   }
 
   Emitter.prototype.hasListeners = function (event) {
-    this.eventHandlerMap = this.eventHandlerMap || new Map()
     const fns = this.eventHandlerMap.get(event)
     return !!(fns && fns.length)
   }
